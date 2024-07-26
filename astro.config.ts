65e4fe51b91cdeb2e388d9a6b6929edb129579b4
Dung Huynh Duc <dunghd.it@gmail.com>
2024-07-26 23:45:07 +0800

    feat: integrate Astro with Vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 import tutorialkit from "@tutorialkit/astro";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-	devToolbar: {
-		enabled: false,
-	},
-	integrations: [tutorialkit()],
+  devToolbar: {
+    enabled: false
+  },
+  integrations: [tutorialkit()],
+  output: "server",
+  adapter: vercel()
 });
